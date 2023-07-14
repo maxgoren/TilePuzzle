d646771e25571d00aa28b40cb7cab7b64892889e
Max Goren <65133477+maxgoren@users.noreply.github.com>
2023-07-14 14:33:54 -0400

    Add files via upload

--- TilePuzzle-PFS-LCRS.cpp
+++ TilePuzzle-PFS-LCRS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_set>
 using namespace std;
 
 typedef vector<vector<int>> Board;
@@ -11,16 +12,20 @@ struct point {
     }
 };
 
+int nodeIdCounter = 0;
+
 //LCRS w/additional parent pointer.
 struct node {
+    int ply;
     Board board;
     point blank;
     node* parent;
     node* next;
     node* children;
-    node(Board info, int x, int y, node* parent, node* next) {
-            this->blank.x = x;
-            this->blank.y = y;
+    node(Board info, point begin, node* parent, node* next) {
+            this->ply = 0;
+            this->blank.x = begin.x;
+            this->blank.y = begin.y;
             this->board = info;
             this->parent = parent;
             this->next = next;
@@ -63,13 +68,14 @@ class SlidePuzzle {
             if (isSafe(next.x, next.y)) {
                 Board board = h->board;
                 swap(board[next.x][next.y], board[h->blank.x][h->blank.y]);
-                h->children = new node(board, next.x, next.y, h, h->children);
+                h->children = new node(board, next, h, h->children);
+                h->children->ply = h->ply + 1;
                 tryCount++;
             }
         }
-        return h;
+        return h->children;
     }
-    point find(Board board, int x) {
+     point find(Board board, int x) {
         for (int i = 0; i < M; i++) {
             for (int j= 0; j < M; j++) {
                 if (x == board[i][j])
@@ -78,17 +84,19 @@ class SlidePuzzle {
         }
         return point(-1,-1);
     }
-    int calculateBoardCost(Board a, Board b) {
+    int getBoardCost(Board a, Board b) {
         int cost = 0;
+        int oop = 0;
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < M; j++) {
                 if (a[i][j] != b[i][j]) {
                    point p = find(b, a[i][j]);
                    cost += std::abs(i - p.x) + std::abs(j - p.y);
+                   oop += 1;
                 }
             }
         }       
-        return cost;
+        return oop;
     }
     void cleanUp(link h) {
         if (h != nullptr) {
@@ -102,21 +110,23 @@ public:
         M = 3;
     }
     typedef pair<int,link> pNode;
-    void PFS(link start, Board goal) {
+    void PFS(Board first, Board goal) {
+        tryCount = 0;
+        link start = new node(first, find(first, 0),nullptr, nullptr);
         priority_queue<pNode, vector<pNode>, greater<pNode>> pq;
         pq.push(make_pair(0, start));
         while (!pq.empty()) {
             link curr = pq.top().second;
             pq.pop();
-            cout<<calculateBoardCost(curr->board, goal)<<" ";
-            if (calculateBoardCost(curr->board, goal) == 0) {
+            cout<<getBoardCost(curr->board, goal)<<" ";
+            if (getBoardCost(curr->board, goal) == 0) {
                 cout<<"Solution Found!\n";
                 showSolution(curr);
                 break;
             }
-            curr = generateChildren(curr);
+            curr->children = generateChildren(curr);
             for (link t = curr->children; t != nullptr; t = t->next) {
-                   pq.push(make_pair(calculateBoardCost(t->board, goal), t));
+                   pq.push(make_pair(getBoardCost(t->board, goal), t));
             }
         }
         cleanUp(start);
@@ -124,18 +134,31 @@ public:
 };
 
 int main() {
-   Board start = {
-                    {2,8,3},
-                    {1,6,4},
-                    {7,0,5} 
-                    };
+   Board p1start = {
+                   {2,8,3},
+                   {1,6,4},
+                   {7,0,5}
+                 };
 
-    Board goal = { 
+    Board p1goal = { 
                    {1,2,3},
                    {8,0,4},
                    {7,6,5}
+                 };
+
+    Board p2start = {
+                    {1, 2, 3},
+                    {5, 6, 0},
+                    {7, 8, 4}
                     };
+ 
+    Board p2goal = {
+                    {1, 2, 3},
+                    {5, 8, 6},
+                    {0, 7, 4}
+                   };
     SlidePuzzle ep;
-    ep.PFS(new node(start, 2,1,nullptr, nullptr), goal);
+    ep.PFS(p1start, p1goal);
+    ep.PFS(p2start, p2goal);
     return 0;
 }
